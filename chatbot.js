6100db380016d8f75d352f927f721d52eba81064
Noedson Lima <seuemail@gmail.com>
2026-02-10 09:44:55 -0300

    fix gitignore session

--- chatbot.js
+++ chatbot.js
@@ -1,11 +1,41 @@
+require("dotenv").config();
+const BOT_ATIVO = process.env.BOT_ATIVO === "true";
 const { Client, LocalAuth } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
 
 const client = new Client({
-  authStrategy: new LocalAuth(),
+  authStrategy: new LocalAuth({
+    dataPath: "./session"
+  }),
   puppeteer: {
+    headless: true,
     args: [
       "--no-sandbox",
-      "--disable-setuid-sandbox"
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage"
     ]
   }
 });
+
+client.on("qr", qr => {
+  console.log("📲 Escaneie o QR Code abaixo:");
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("✅ Bot conectado com sucesso!");
+});
+
+client.on("message", async msg => {
+  if (!BOT_ATIVO) return; // DESLIGAR O BOT
+  if (msg.from.includes("@g.us")) return; // ingnora grupos
+  if (!msg.body) return;
+
+  console.log("📩 Mensagem recebida:", msg.body);
+
+  if (msg.body.toLowerCase() === "oi") {
+    await msg.reply("Oi 👋 Estou online e funcionando!");
+  }
+});
+
+client.initialize();
